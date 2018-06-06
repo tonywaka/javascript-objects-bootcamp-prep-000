c391415000a4e0c8cf267c9c45c5fd64a3373a4d
Anthony Udezi <tonywaka@uniben.edu>
2018-06-06 21:27:57 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -10,6 +10,6 @@ updatePlaylist(playlist,'Phil Ochs', "Here's to the State of Mississippi")
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName;
-  return playlist
+  console.log(playlist);
 }
 removeFromPlaylist(playlist, 'Slowdive')
